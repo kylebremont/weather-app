74c5f6ee87d25a14cf9378b798f44ca71c02818b
Kyle Bremont <kylebremont@gmail.com>
2020-04-27 14:44:03 -0600

    Parsed json data into mongodb

--- database/setup_mongo.py
+++ database/setup_mongo.py
@@ -9,7 +9,7 @@ class DBSetup():
     def __init__(self):
         self.token = ''
         self.url = 'https://api.weatherbit.io/v2.0/forecast/daily?'
-        self.query = 'city=Boulder,CO&units=I&days=3&key='
+        self.query = 'city=Boulder,CO&units=I&days=7&key='
 
     def setup_client(self):
         self.client = pymongo.MongoClient("mongodb+srv://kylebremont:weatherapp@cluster0-t12le.gcp.mongodb.net/test?retryWrites=true&w=majority")
@@ -20,11 +20,21 @@ class DBSetup():
         self.weather_collection = self.weather_db["weather"]
 
     def load_collections(self):
+        weather_dict = {}
         response = requests.get(self.url + self.query + self.token)
+
         if response.status_code == 200:
             data_json = json.loads(response.content.decode('utf-8'))
-            pprint.pprint(data_json)
-            # self.weather_collection.insert(data_json)
+            data_json = data_json['data']
+
+            for i in range(len(data_json)):
+                date = data_json[i]['valid_date']
+                high_temp = data_json[i]['max_temp']
+                low_temp = data_json[i]['min_temp']
+                precip = data_json[i]['precip']
+                weather_dict[date] = [{'high_temp': high_temp}, {'low_temp': low_temp}, {'precip': precip}]
+
+            self.weather_collection.insert_one(weather_dict)
         else:
             print('Error!')
 
@@ -37,11 +47,11 @@ class DBSetup():
 
 def main():
     db = DBSetup()
-    # db.setup_client()
-    # db.setup_collections()
-    # db.drop_data()
+    db.setup_client()
+    db.setup_collections()
+    db.drop_data()
     db.load_collections()
-    # db.tear_down()
+    db.tear_down()
 
 if __name__=='__main__':
     main()
--- server.js
+++ server.js
@@ -1,12 +1,26 @@
+const {MongoConnector, MongoDB} = require('./server/facade.js');
 const express = require('express');
 const app = express();
 const port = 3001;
 
 // allow cross-origin requests
 let allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    res.header('Access-Control-Allow-Headers', "*");
-    next();
-  }
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Headers', "*");
+  next();
+}
 app.use(allowCrossDomain);
+
+var connector = new MongoConnector();
+
+connector.connect(function(err, db) {
+  db = new MongoDB(db);
+
+  app.get('/temps', (req, res) => {
+    db.get(req.query.type, query, function(results) {
+      res.status(200).send(results);
+    });
+  });
+  
+});
 
